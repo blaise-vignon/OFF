044f1d42cfb8516401772f198ef74b9ede6efafc
BlaiseV <blaisev@live.ie>
2013-11-06 23:35:09 +0100

    compiles and scan!!

---
+++ BarCodeScanner/PhotoCameraLuminanceSource.cs
@@ -0,0 +1,31 @@
+﻿using ZXing;
+
+namespace BarcodeScanner
+{
+   public class PhotoCameraLuminanceSource : BaseLuminanceSource
+   {
+      public byte[] PreviewBufferY
+      {
+         get
+         {
+            return luminances;
+         }
+      }
+
+      public PhotoCameraLuminanceSource(int width, int height)
+         : base(width, height)
+      {
+      }
+
+      internal PhotoCameraLuminanceSource(int width, int height, byte[] newLuminances)
+         : base(width, height)
+      {
+         luminances = newLuminances;
+      }
+
+      protected override LuminanceSource CreateLuminanceSource(byte[] newLuminances, int width, int height)
+      {
+         return new PhotoCameraLuminanceSource(width, height, newLuminances);
+      }
+   }
+}
